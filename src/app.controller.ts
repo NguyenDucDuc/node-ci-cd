74c31063e159a75acc75b7a2056ea2e4491862fd
nguyenducduc <nguyenducduc26797@gmail.com>
2023-08-10 17:02:36 +0700

    init

---
+++ src/app.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get, Res } from '@nestjs/common';
+import { AppService } from './app.service';
+import { Response } from 'express';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Get('/hello1')
+  async(@Res() res: Response) {
+    return res.status(200).send('hello 1');
+  }
+}
